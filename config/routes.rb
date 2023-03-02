34cdf24d842547c82fec7e1458dcec52eb41839c
sourabhWK <sourabh@webkorps.com>
2023-03-02 19:49:58 +0530

    resolved errors from routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,9 @@ Rails.application.routes.draw do
   get 'admin', to: 'admin#index'
   devise_for :users
 
-
-  # post '/questions/:id/answers/new', to: 'answers#create'
-
-
   root "home#index"
 
   resources :questions do 
     resources :answers
   end
-
-
-  root "home#index"
-  resources :questions
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
 end
