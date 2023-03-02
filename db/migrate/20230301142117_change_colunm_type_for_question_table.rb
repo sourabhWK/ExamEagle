02e491249db2aad55bdbd022290cc8b882fed43a
nitish choudhary <nitish@webkorps.com>
2023-03-02 20:45:34 +0530

    changes in databse

--- db/migrate/20230301142117_change_colunm_type_for_question_table.rb
+++ db/migrate/20230301142117_change_colunm_type_for_question_table.rb
@@ -1,7 +1,4 @@
 class ChangeColunmTypeForQuestionTable < ActiveRecord::Migration[7.0]
   def change
-    change_column(:questions, :language, :integer)
-    change_column(:questions, :q_type, :integer)
-    change_column(:questions, :q_level, :integer)
   end
 end
