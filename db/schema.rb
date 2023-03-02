29f27b1bfe198283cb7ec2c5a0f5177b937e40c0
sourabhWK <sourabh@webkorps.com>
2023-03-02 19:48:42 +0530

    resolved errors

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_175630) do
     t.boolean "correct"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
+  end
+end
 ActiveRecord::Schema[7.0].define(version: 2023_03_01_144705) do
   create_table "languages", force: :cascade do |t|
     t.string "name"
