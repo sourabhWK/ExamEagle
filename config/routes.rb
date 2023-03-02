596eeacae2173bc3ad41e67803dc956e483d419f
Sourabh Kourav <115283432+sourabhWK@users.noreply.github.com>
2023-03-02 20:31:45 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,4 @@ Rails.application.routes.draw do
     resources :answers
   end
 
-
-
-  
-  resources :questions
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-
-
 end
